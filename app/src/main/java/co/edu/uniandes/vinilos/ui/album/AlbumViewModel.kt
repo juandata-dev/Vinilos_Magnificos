40116b606a2f15e2408791103102abfbff52e41f
Felix E. Orduz G. <felixernestoorduzgrimaldo@Felixs-MacBook-Pro.local>
2023-11-04 15:21:43 -0500

    feat(album): add AlbumAdapter and AlbumFragment classes
    The AlbumAdapter class is responsible for binding album data to the list view and handling album selection events. It includes methods for updating the album list and handling album selection.
    
    The AlbumFragment class represents the fragment for displaying albums. It includes a ViewModel for managing the business logic of the view. The fragment inflates the layout file, sets up the adapter for the album list, and observes changes in the album list data.
    
    The AlbumViewModel class handles communication with the server to retrieve album data. It includes methods for getting all albums and updating the album list LiveData. The ViewModel is responsible for retrieving the data on a background thread using coroutines.
    
    The changes were made to implement the functionality of displaying albums and handling album selection events in the application.

---
+++ app/src/main/java/co/edu/uniandes/vinilos/ui/album/AlbumViewModel.kt
@@ -0,0 +1,50 @@
+package co.edu.uniandes.vinilotunes.ui.album
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import co.edu.uniandes.vinilotunes.data.model.Album
+import co.edu.uniandes.vinilotunes.data.repository.AlbumRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+ * Esta clase es la encargada de manejar la comunicación con el servidor para obtener los datos de los álbumes.
+ * @property application La aplicación que se está ejecutando.
+ */
+class AlbumViewModel(application: Application) : AndroidViewModel(application) {
+
+    // Utilizamos MutableLiveData para poder cambiar su valor dinámicamente. En este caso, el valor de la variable es una lista de álbumes.
+    val listAlbums = MutableLiveData<List<Album>>()
+
+    // Utilizamos MutableLiveData para poder cambiar su valor dinámicamente. En este caso, el valor de la variable es un álbum.
+    val album = MutableLiveData<Album>()
+
+    // Variable que almacena el repositorio de álbumes. El repositorio es el encargado de manejar la comunicación con el servidor.
+    private val albumsRepository = AlbumRepository(application)
+
+    // Variable que almacena el texto que se muestra en la vista. En este caso, el texto es "This is album Fragment".
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is album Fragment"
+    }
+
+    // LiveData es una clase que se utiliza para notificar a las vistas cuando los datos cambian. En este caso, el valor de la variable es un texto.
+    val text: LiveData<String> = _text
+
+
+    /**
+     * Función que obtiene todos los álbumes. Esta función se ejecuta en un hilo secundario.
+     */
+    fun getAlbums() {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {// Corutina que se ejecuta en un hilo secundario.
+                listAlbums.postValue(albumsRepository.getAllAlbums()) // Se obtienen los álbumes y se actualiza el valor de la variable.
+            }
+        } catch (e: Exception) {
+            Log.e("Error", e.message ?: "Failure service")
+        }
+    }
+}
