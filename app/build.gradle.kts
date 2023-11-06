263943968b460a0b56cca60135f030ebceac7f6c
dbuelvasc <d.buelvasc@uniandes.edu.co>
2023-11-05 23:10:35 -0500

    Update build.gradle.kts
    add new dependencies to file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,21 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("de.mannodermaus.android-junit5") version "1.8.0.0"
 }
 
 android {
-    namespace = "co.edu.uniandes.vinilos"
+    namespace = "co.edu.uniandes.vinilotunes"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "co.edu.uniandes.vinilos"
+        applicationId = "co.edu.uniandes.vinilotunes"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
+        namespace = "co.edu.uniandes.vinilotunes"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -34,6 +36,20 @@ android {
         jvmTarget = "1.8"
     }
     buildToolsVersion = "34.0.0"
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    testOptions {
+        unitTests.apply {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
 
 dependencies {
@@ -42,7 +58,31 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.databinding:databinding-runtime:8.1.2")
+    implementation("com.github.bumptech.glide:glide:5.0.0-rc01")
+//    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation("com.squareup.okhttp3:okhttp:4.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("org.mockito:mockito-core:3.12.4")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation("androidx.fragment:fragment-testing:1.6.2")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test:rules:1.4.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:5.0.0-rc01")
+    kapt("com.github.bumptech.glide:compiler:5.0.0-rc01")
 }
